efe6e436775bc08c189119ced298fee0fcf72b4e
Tushar Saini <90309936+Tushar9352@users.noreply.github.com>
2023-05-20 10:44:07 +0530

    Create Perfect_Number.java

---
+++ Perfect_Number.java
@@ -0,0 +1,20 @@
+   import java.util.Scanner;
+class PerfectNumber
+{
+    public static void main(String[] args) 
+    {
+        int n, sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number you want to check:");
+        n = s.nextInt();
+for(int i = 1; i < n; i++)
+        {
+            if(n % i == 0)
+            {
+                sum = sum + i;
+            }
+        }
+        if(sum == n)
+        {
+            System.out.println("Given number is Perfect");
+        }
